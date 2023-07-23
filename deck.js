cb23d98d8fdb2984bba9fc11fa2f9068482b5677
clampandgo <ganeshrameras911@gmail.com>
2023-07-23 14:05:58 -0500

    Update deck.js

---
+++ deck.js
@@ -0,0 +1,15 @@
+const SUITS =
+
+class Deck (
+    constructor () {
+        this.cards = cards
+    }
+)
+
+class Card {
+    constructor(suit, value) {
+        this.suit = suit
+        this.value = value
+    }
+
+}
